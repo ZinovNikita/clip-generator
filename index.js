c47686d464aa2bfbb43abc85c4f5cc8b923befcd
Zinov Nikita <zinov.nikita2010@yandex.ru>
2021-03-02 02:12:35 +0500

    Изменениея алгоритма сегментации

--- index.js
+++ index.js
@@ -25,31 +25,31 @@ const Segmentation = ()=>{
         let v = a[i];
         document.querySelector('#segmentation-body .card-subtitle').innerHTML = './input/'+v;
         ffmpeg('./input/'+v).noAudio().size('1280x720').fps(25)
-        .outputOptions(['-force_key_frames expr:gte(t,n_forced)','-map 0','-segment_time 1.0','-f segment','-reset_timestamps 1'])
+        .outputOptions(['-force_key_frames expr:gte(t,n_forced)','-map 0','-segment_time 0.5','-f segment','-reset_timestamps 1'])
+        .on('progress', function(progress) {
+            doneCount += progress.percent/100;
+            let p = (doneCount/a.length * 100).toFixed(2)+'%';
+            document.querySelector('#segmentation-body .progress-bar').style.width = p;
+            document.querySelector('#segmentation-body .card-title').innerHTML = p;
+        })
         .on('error',err=>{
             document.querySelector('#segmentation-body').innerHTML += '<span class="badge badge-danger">Error '+v+': ' + err.message+'</span><br>';
             console.error(err);
             if(i<a.length-1) recurcy(++i);
         })
         .on('end',()=>{
-            doneCount++;
-            let p = (Math.round(doneCount/a.length * 1000)/10)+'%';
-            document.querySelector('#segmentation-body .progress-bar').style.width = p;
-            document.querySelector('#segmentation-body .card-title').innerHTML = p;
-            console.log(doneCount,a.length,doneCount==a.length)
-            if(doneCount==a.length)
+            if(i==a.length-1)
                 document.querySelector('#segmentation-body button.btn').removeAttribute('disabled');
             document.querySelector('#segmentation-body').innerHTML += '<span class="badge badge-success">'+v+' finished!</span><br>';
             if(i<a.length-1) recurcy(++i);
         })
         .save('./segments/'+i+'_'+(new Date().getTime())+'%5d.mp4');
     };
-    recurcy(0);
+    recurcy(0,0.1);
 }
 const Clipping = ()=>{
     let m = fs.readdirSync("./audio");
     if(!m||m.length==0) return alert('Empty ./audio folder');
-    let fls = fs.readdirSync("./segments");
     let doneCount = 0;
     document.querySelector('#clipping-body .card-title').innerHTML = '0%';
     document.querySelector('#clipping-body button.btn').setAttribute('disabled',true);
@@ -57,13 +57,29 @@ const Clipping = ()=>{
         let a = m[i];
         new WebAudioAPI.AudioContext().decodeAudioData(fs.readFileSync("./audio/"+a).buffer,audioBuffer=>{
             document.querySelector('#clipping-body .card-subtitle').innerHTML = './audio/'+a;
+            let pcmdata = audioBuffer.getChannelData(0).map(el=>{return Math.abs(el)});
+            let count = 0;
+            let max = 0;
+            pcmdata.forEach(n=>{max = n > max ? n : max});
+            pcmdata.forEach(n=>{
+                if(n > (max-0.3) && n < (max+0.3))
+                    count++;
+            });
+            let bpm = Math.ceil(10/(count/audioBuffer.sampleRate))/10;
             let dur = Math.ceil(audioBuffer.duration);
 			var fn = new Date().getTime();
-            let ff = ffmpeg();
+            let ff = [];
+            let fls = fs.readdirSync("./segments");
 			for(var j=0;j<=dur;j++)
-                ff.input('./segments/'+fls[rndInt(0,fls.length)]);
-            ff.on('error',err=>{
-                document.querySelector('#clipping-body').innerHTML += '<span class="badge badge-danger">Error clipping '+a+'</span><br>';
+                ff.push("file 'C:/Users/zinov/Documents/mugen2/segments/"+fls[rndInt(0,fls.length-1)]+"'");
+            fs.writeFileSync("./list.txt", ff.join('\n'));
+            ffmpeg('list.txt').inputOptions(['-f concat','-safe 0']).outputOptions(['-c copy'])
+            .on('progress', function(progress) {
+                console.log('Processing concat: ' + progress.percent + '% done');
+            })
+            .on('error',err=>{
+                document.querySelector('#clipping-body').innerHTML += '<span class="badge badge-danger">Error clipping '+a+': '+err.message+'</span><br>';
+                document.querySelector('#clipping-body button.btn').removeAttribute('disabled');
                 console.error(err);
             })
             .on('end',()=>{
@@ -73,8 +89,11 @@ const Clipping = ()=>{
                 document.querySelector('#clipping-body .card-title').innerHTML = p;
                 document.querySelector('#clipping-body').innerHTML += '<span class="badge badge-success">Clipping for '+a+' finished!</span><br>';
                 ffmpeg('./tmp/'+fn+'.mp4').input("./audio/"+a).outputOptions(['-ss 00:00:00','-t '+durationString(dur)])
+                .on('progress', function(progress) {
+                    console.log('Processing merge: ' + progress.percent + '% done');
+                })
                 .on('error',err=>{
-                    document.querySelector('#clipping-body').innerHTML += '<span class="badge badge-danger">Error merging '+a+' with '+fn+'.mp4</span><br>';
+                    document.querySelector('#clipping-body').innerHTML += '<span class="badge badge-danger">Error merging '+a+' with '+fn+'.mp4: '+err.message+'</span><br>';
                     console.error(err);
                     if(i<m.length-1) recurcy(++i);
                 })
@@ -86,12 +105,26 @@ const Clipping = ()=>{
                     if(doneCount==m.length*2)
                         document.querySelector('#clipping-body button.btn').removeAttribute('disabled');
                     document.querySelector('#clipping-body').innerHTML += '<span class="badge badge-success">Merging '+fn+'.mp4 finished!</span><br>';
+                    fs.unlinkSync('./tmp/'+fn+'.mp4');
                     if(i<m.length-1) recurcy(++i);
                 })
                 .save('./results/'+fn+'.mp4');
             })
-            .mergeToFile('./tmp/'+fn+'.mp4');
+            .save('./tmp/'+fn+'.mp4');
         })
     };
     recurcy(0);
+}
+const getBPM = (path)=>{
+    new WebAudioAPI.AudioContext().decodeAudioData(fs.readFileSync(path).buffer,audioBuffer=>{
+        let pcmdata = audioBuffer.getChannelData(0).map(el=>{return Math.abs(el)});
+        let count = 0;
+        let max = 0;
+        pcmdata.forEach(n=>{max = n > max ? n : max});
+        pcmdata.forEach(n=>{
+            if(n > (max-0.3) && n < (max+0.3))
+                count++;
+        });
+        console.log(Math.ceil(count/audioBuffer.sampleRate*60));
+    })
 }
--- main.js
+++ main.js
@@ -10,12 +10,13 @@ const createWindow = ()=>{
         }
     })
     win.loadFile('index.html');
-    //win.webContents.openDevTools();
+    win.webContents.openDevTools();
     if (!fs.existsSync('./audio')) fs.mkdirSync('./audio');
     if (!fs.existsSync('./input')) fs.mkdirSync('./input');
     if (!fs.existsSync('./results')) fs.mkdirSync('./results');
     if (!fs.existsSync('./segments')) fs.mkdirSync('./segments');
     if (!fs.existsSync('./tmp')) fs.mkdirSync('./tmp');
+    if (!fs.existsSync('./tmp/tmp')) fs.mkdirSync('./tmp/tmp');
 }
 app.whenReady().then(createWindow)
 app.on('window-all-closed', () => {if (process.platform !== 'darwin') app.quit()});
